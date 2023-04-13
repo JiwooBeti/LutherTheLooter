d92b42cbe35edd84971848672448e9f4384475f7
Jaideepputturi <128713092+Jaideepputturi@users.noreply.github.com>
2023-04-13 08:26:44 -0500

    Animation run

--- LutherTheLooter/Assets/DefaultEnemy.cs
+++ LutherTheLooter/Assets/DefaultEnemy.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DefaultEnemy : MonoBehaviour
 {
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] int index = 0;
+    [SerializeField] float speed;
+    [SerializeField] float maxRotation;
+    [SerializeField] float smallestRotation=.5f;
+    [SerializeField] float proximity;
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,12 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 newLoc = Vector2.MoveTowards(transition.position, waypoints[index].position, .3f);
+        transform.position = newLoc;
+
+        if (MathF.Abs(transform.position.x - waypoints[index].position.x) < .01f && Mathf.Abs(transform.position.y- waypoints[index].position.y)< .01f)
+        {
+            index = (index + 1) % waypoints.Count;
+        }
     }
 }
